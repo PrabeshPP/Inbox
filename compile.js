1e518ed8cc6b6d68dd67253e36e1643dccbecd55
Prabesh <prabesh.bista369@gmail.com>
2022-01-17 13:27:46 +0545

    compiler for inbox.sol

--- compile.js
+++ compile.js
@@ -1,8 +1,11 @@
 let path=require('path');
 let fs=require('fs');
-let solc=require('solc');
+const { isNumberObject } = require('util/types');
+const newLocal = 'solc';
+let solc=require(newLocal)
 
 let inboxPath=path.resolve(__dirname,'contracts','Inbox.sol');
 let source=fs.readFileSync(inboxPath,'utf-8');
+console.log(inboxPath);
 
 console.log(solc.compile(source,1));
