a2806dd717e75b3e556c2ae56a01681ee0071d9e
Prabesh <prabesh.bista369@gmail.com>
2022-01-17 14:19:46 +0545

    compiler

--- compile.js
+++ compile.js
@@ -1,11 +1,39 @@
 let path=require('path');
 let fs=require('fs');
-const { isNumberObject } = require('util/types');
 const newLocal = 'solc';
 let solc=require(newLocal)
 
 let inboxPath=path.resolve(__dirname,'contracts','Inbox.sol');
 let source=fs.readFileSync(inboxPath,'utf-8');
-console.log(inboxPath);
 
-console.log(solc.compile(source,1));
+
+var input={
+    lanaguage:'Solidity',
+    sources:{
+        source:{
+            content:'import "lib.sol"; contract C { function f() public { L.f(); } }'
+        }
+    },
+    settings:{
+        outputSelection: {
+            '*': {
+              '*': ['*']
+            }
+          }
+    }
+};
+
+function findImports(path) {
+    if (path === 'lib.sol')
+      return {
+        contents:
+          'library L { function f() internal returns (uint) { return 7; } }'
+      };
+    else return { error: 'File not found' };
+  }
+
+  var output = JSON.parse(
+    solc.compile(JSON.stringify(input), { import: findImports })
+  );
+
+  console.log(output);
