ce0135909810dadf7435e6213ffcabc778967efe
Prabesh <prabesh.bista369@gmail.com>
2022-01-30 20:30:49 +0545

    deplyoed contract

--- deploy.js
+++ deploy.js
@@ -3,7 +3,7 @@ const Web3=require('web3');
 const{abi,evm}=require('./compile');
 const bytecode=evm.bytecode.object;
 let accounts;
-let inbox;
+let result;
 
 /* in the HDWalletProvide we have pass two seprate arguments first-one our ac. pneumonic,
  and the second one should be the link provide by the Infura
@@ -22,6 +22,20 @@ const web3=new Web3(provider);
 
 const deploy=async()=>{
     accounts=await web3.eth.accounts;
+    console.log("Attempting to demploy from the account ",accounts[0]);
+    result=await web3.eth.Contract(abi).deploy(
+        {
+            data:bytecode,
+            arguments:["Hi Prabesh!"]
+        }
+    ).send({
+        from:accounts[0],
+        gas:"1000000"
+    });
+    console.log("Conteact deployed to ",result.options.address);
+    inbox.setProvider(provider);
     
     
 }
+
+deploy();
