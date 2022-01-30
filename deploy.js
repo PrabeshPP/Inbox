d2b155007e43a9ae50efc582fd1342fb6fa65652
Prabesh <prabesh.bista369@gmail.com>
2022-01-30 20:18:36 +0545

    deploying the contract

--- deploy.js
+++ deploy.js
@@ -6,8 +6,12 @@ const bytecode=evm.bytecode.object;
 /* in the HDWalletProvide we have pass two seprate arguments first-one our ac. pneumonic,
  and the second one should be the link provide by the Infura
 */
+const mnemonicPhrase="cancel evil glimpse lock riot capable crumble cliff ordinary leaf float taxi";
+const provider=new HDWalletProvider({
+    mnemonic:{phrase:mnemonicPhrase},
+    providerOrUrl:"https://rinkeby.infura.io/v3/bd79d348176343c596b09fb772c7aa0c" 
+})
 
-const provider=new HDWalletProvider(
-    "cancel evil glimpse lock riot capable crumble cliff ordinary leaf float taxi",
-    "https://rinkeby.infura.io/v3/bd79d348176343c596b09fb772c7aa0c" 
-)
+// web3
+
+const web3=new Web3(provider);
