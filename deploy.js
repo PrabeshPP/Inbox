dbf078c08d521e2e02534bf4bb1c283926a45180
Prabesh <prabesh.bista369@gmail.com>
2022-01-30 21:17:56 +0545

    .

--- deploy.js
+++ deploy.js
@@ -2,17 +2,17 @@ const HDWalletProvider=require("@truffle/hdwallet-provider");
 const Web3=require('web3');
 const{abi,evm}=require('./compile');
 const bytecode=evm.bytecode.object;
-let accounts;
-let result;
 
 /* in the HDWalletProvide we have pass two seprate arguments first-one our ac. pneumonic,
  and the second one should be the link provide by the Infura
 */
 const mnemonicPhrase="cancel evil glimpse lock riot capable crumble cliff ordinary leaf float taxi";
-const provider=new HDWalletProvider({
-    mnemonic:{phrase:mnemonicPhrase},
-    providerOrUrl:"https://rinkeby.infura.io/v3/bd79d348176343c596b09fb772c7aa0c" 
-})
+let provider=new HDWalletProvider({
+    mnemonic:{phrase:
+        mnemonicPhrase},
+    providerOrUrl:"https://rinkeby.infura.io/v3/bd79d348176343c596b09fb772c7aa0c",
+   
+});
 
 // web3
 
@@ -21,19 +21,14 @@ const web3=new Web3(provider);
 //async-await function
 
 const deploy=async()=>{
-    accounts=await web3.eth.accounts;
-    console.log("Attempting to demploy from the account ",accounts[0]);
-    result=await web3.eth.Contract(abi).deploy(
-        {
-            data:bytecode,
-            arguments:["Hi Prabesh!"]
-        }
-    ).send({
-        from:accounts[0],
-        gas:"1000000"
-    });
+    const accounts=await web3.eth.getAccounts();
+    console.log("Attempting to deploy from the account ",accounts[0]);
+    const result=await new web3.eth.Contract(abi)
+    .deploy({data:bytecode,arguments:["Hi there!"]})
+    .send({from:accounts[0],gas:'1000000'});
+
     console.log("Conteact deployed to ",result.options.address);
-    inbox.setProvider(provider);
+   
     
     
 }
