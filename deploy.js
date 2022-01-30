4bc19f0aaaa9f10fccf9c4c63925a4057ea122f3
Prabesh <prabesh.bista369@gmail.com>
2022-01-30 20:09:47 +0545

    deploying the contract

--- deploy.js
+++ deploy.js
@@ -2,3 +2,11 @@ const HDWalletProvider=require("@truffle/hdwallet-provider");
 const Web3=require('web3');
 const{abi,evm}=require('./compile');
 const bytecode=evm.bytecode.object;
+
+/* in the HDWalletProvide we have pass two seprate arguments first-one our ac. pneumonic,
+ and the second one should be the link provide by the Infura
+*/
+
+const provider=new HDWalletProvider(
+    "deploying the contract"
+)
