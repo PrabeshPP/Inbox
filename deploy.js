73a5692f7c99ca22d68c78cb613bc229e5de8ea8
Prabesh <prabesh.bista369@gmail.com>
2022-01-30 20:06:15 +0545

    deploying the contract

--- deploy.js
+++ deploy.js
@@ -1,4 +1,4 @@
-const HDWallet=require("@truffle/hdwallet-provider");
+const HDWalletProvider=require("@truffle/hdwallet-provider");
 const Web3=require('web3');
 const{abi,evm}=require('./compile');
 const bytecode=evm.bytecode.object;
